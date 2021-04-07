4d70b55604bec7ef181ff84ebf521b5b1f95b498
Julian Kornberger <jk+github@digineo.de>
2021-04-07 08:00:26 +0200

    Start with generating XML

--- lib/xrechnung.rb
+++ lib/xrechnung.rb
@@ -1,6 +1,67 @@
 require "xrechnung/version"
+require "builder"
 
 module Xrechnung
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Document
+
+    attr_accessor :id, :issue_date, :due_date, \
+      :invoice_type_code, :document_currency_code, :note,
+      :order_reference_id
+
+    attr_accessor :supplier, :customer
+
+    def initialize
+      self.invoice_type_code      = 380
+      self.document_currency_code = "EUR"
+    end
+
+    def to_xml(indent: 2, target: "")
+      xml = Builder::XmlMarkup.new(indent: indent, target: target)
+      xml.instruct! :xml, version: "1.0", encoding: "UTF-8"
+
+      xml.Invoice \
+        "xmlns:cac" => "urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2",
+        "xmlns:cbc" => "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2",
+        "xmlns" => "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2" do
+        xml.cbc :CustomizationID, "urn:cen.eu:en16931:2017#compliant#urn:fdc:peppol.eu:2017:poacc:billing:3.0"
+        xml.cbc :ProfileID, "urn:fdc:peppol.eu:2017:poacc:billing:01:1.0"
+        xml.cbc :ID, id
+        xml.cbc :IssueDate, issue_date
+        xml.cbc :DueDate, due_date
+        xml.cbc :InvoiceTypeCode, invoice_type_code
+        xml.cbc :DocumentCurrencyCode, document_currency_code
+        xml.cbc :Note, note
+
+        xml.cac :OrderReference do
+          xml.cbc :ID, order_reference_id
+        end
+
+        xml.cac :AccountingSupplierParty do
+          supplier&.to_xml(xml)
+        end
+        xml.cac :AccountingCustomerParty do
+          customer&.to_xml(xml)
+        end
+      end
+
+      target
+    end
+
+  end
+
+
+  class Party
+    attr_accessor :name
+
+    def to_xml(xml)
+      xml.cac :Party do
+        xml.cac :PartyName do
+          xml.cbc :Name, name
+        end
+      end
+    end
+  end
+
 end
--- spec/xrechnung_spec.rb
+++ spec/xrechnung_spec.rb
@@ -1,9 +1,31 @@
+require "date"
+
 RSpec.describe Xrechnung do
   it "has a version number" do
     expect(Xrechnung::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "generates XML" do
+    doc = Xrechnung::Document.new
+    doc.id         = "ERB_UBL_INVOICE_001"
+    doc.issue_date = Date.parse("2020-11-13")
+    doc.due_date   = Date.parse("2020-12-01")
+    doc.note       = "Das ist ein globaler Kommentar zur Rechnung.\nAuch Zeilenumbrüche in einer Note werden berücksichtigt."
+
+    doc.order_reference_id = 1234567890
+
+    doc.customer   = Xrechnung::Party.new
+    doc.customer.name = "BRZ GmbH"
+
+    doc.supplier   = Xrechnung::Party.new
+    doc.supplier.name = "Mustermann GmbH"
+
+
+    expected = File.read("spec/fixtures/xrechnung.xml")
+
+    # Remove XML comments
+    expected.gsub!(/\s*<!--.+?-->/mi, "")
+
+    expect(doc.to_xml).to eq(expected)
   end
 end
