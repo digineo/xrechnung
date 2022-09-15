d4102d068c0971d32ba03f428a9de7974a94ead1
florian2 <fgoersdorf@gmail.com>
2022-09-15 17:52:42 +0200

    adds CompanyID to PartyLegalEntity

--- lib/xrechnung/party_legal_entity.rb
+++ lib/xrechnung/party_legal_entity.rb
@@ -6,10 +6,15 @@ module Xrechnung
     #   @return [String]
     member :registration_name, type: String
 
+    # @!attribute company_id
+    #   @return [String]
+    member :company_id, type: String
+
     # noinspection RubyResolve
     def to_xml(xml)
       xml.cac :PartyLegalEntity do
         xml.cbc :RegistrationName, registration_name
+        xml.cbc :CompanyID, company_id
       end
     end
   end
