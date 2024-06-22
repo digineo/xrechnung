d56bf5f00a48c6f3dc6f54ea810f5d1bc28d0892
Stas SUȘCOV <stas@nerd.ro>
2024-06-22 14:53:08 +0100

    PartyLegalEntity: added CompanyID.

--- spec/fixtures/ruby/party_legal_entity.rb
+++ spec/fixtures/ruby/party_legal_entity.rb
@@ -1,5 +1,6 @@
-def build_party_legal_entity
+def build_party_legal_entity(company_id: nil)
   Xrechnung::PartyLegalEntity.new(
+    company_id:        company_id,
     registration_name: "Harry Hirsch Holz- und Trockenbau",
   )
 end
