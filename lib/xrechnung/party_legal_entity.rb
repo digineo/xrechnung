9ff273bf955e513f2665e35a7f9bfa37f974fe16
Stas SUȘCOV <stas@nerd.ro>
2024-06-22 14:53:08 +0100

    PartyLegalEntity: added CompanyID.

--- lib/xrechnung/party_legal_entity.rb
+++ lib/xrechnung/party_legal_entity.rb
@@ -2,6 +2,10 @@ module Xrechnung
   class PartyLegalEntity
     include MemberContainer
 
+    # @!attribute company_id
+    #   @return [String]
+    member :company_id, type: String
+
     # @!attribute registration_name
     #   @return [String]
     member :registration_name, type: String
@@ -9,6 +13,7 @@ module Xrechnung
     # noinspection RubyResolve
     def to_xml(xml)
       xml.cac :PartyLegalEntity do
+        xml.cbc(:CompanyID, company_id) unless company_id.nil?
         xml.cbc :RegistrationName, registration_name
       end
     end
--- spec/xrechnung/party_legal_entity_spec.rb
+++ spec/xrechnung/party_legal_entity_spec.rb
@@ -3,6 +3,9 @@ load("spec/fixtures/ruby/party_legal_entity.rb")
 
 RSpec.describe Xrechnung::PartyLegalEntity do
   it "generates xml" do
-    expect_xml_eq_fixture(build_party_legal_entity, "party_legal_entity")
+    expect_xml_eq_fixture(
+      build_party_legal_entity(company_id: "DE 214365879"),
+      "party_legal_entity"
+    )
   end
 end
