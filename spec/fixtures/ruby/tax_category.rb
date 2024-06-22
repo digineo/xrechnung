fe0e21280aedb69c8a316ddcef8c0c9703c1c16b
Stas SUȘCOV <stas@nerd.ro>
2024-06-22 14:53:53 +0100

    TaxCategory: added exempt attributes.

--- spec/fixtures/ruby/tax_category.rb
+++ spec/fixtures/ruby/tax_category.rb
@@ -1,7 +1,14 @@
-def build_tax_category
-  Xrechnung::TaxCategory.new(
+def build_tax_category(exempt: false)
+  category = Xrechnung::TaxCategory.new(
     id:            "S",
     percent:       16,
     tax_scheme_id: "VAT",
   )
+
+  return category unless exempt
+
+  category.tax_exemption_reason_code = "VATEX-EU-S"
+  category.tax_exemption_reason      = "Exempt"
+
+  category
 end
