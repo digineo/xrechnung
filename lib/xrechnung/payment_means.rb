bee9cfb360ac5f56e85e7e37e68026ff05c7d0ca
Vladimir Glusic <vladimir.glusic@visable.com>
2025-01-16 21:12:46 +0100

    Add InstructionNote and PaymentID
    Extend the payment means with optional fields:
    * BT-82: InstructionNote
    * BT-83: PaymentID

--- lib/xrechnung/payment_means.rb
+++ lib/xrechnung/payment_means.rb
@@ -1,6 +1,8 @@
 module Xrechnung
   # <cac:PaymentMeans>
   #   <cbc:PaymentMeansCode>30</cbc:PaymentMeansCode>
+  #   <cbc:InstructionNote>Textvermerk der Zahlung</cbc:InstructionNote>
+  #   <cbc:PaymentID>Verwendungszweck</cbc:PaymentID>
   #   <cac:PayeeFinancialAccount>
   #     <cbc:ID>DE12500105170648489890</cbc:ID>
   #     <cbc:Name>Harry Hirsch</cbc:Name>
@@ -12,14 +14,32 @@ module Xrechnung
   class PaymentMeans
     include MemberContainer
 
+    # Payment means type code BT-81
+    #
     # @!attribute payment_means_code
     #   @return [Integer]
     member :payment_means_code, type: Integer
 
+    # Payment means text BT-82
+    #
+    # @!attribute payment_means_code
+    #   @return [String]
+    member :instruction_note, type: String, optional: true
+
+    # Remittance information BT-83
+    #
+    # @!attribute payment_means_code
+    #   @return [String]
+    member :payment_id, type: String, optional: true
+
+    # Credit transfer BG-17
+    #
     # @!attribute payee_financial_account
     #   @return [Xrechnung::PayeeFinancialAccount]
     member :payee_financial_account, type: Xrechnung::PayeeFinancialAccount
 
+    # Direct debit BG-19
+    #
     # @!attribute payment_mandate
     #   @return [Xrechnung::PaymentMandate]
     member :payment_mandate, type: Xrechnung::PaymentMandate, optional: true
@@ -27,6 +47,8 @@ module Xrechnung
     # noinspection RubyResolve
     def to_xml(xml)
       xml.cbc :PaymentMeansCode, payment_means_code
+      xml.cbc :InstructionNote, instruction_note if instruction_note
+      xml.cbc :PaymentID, payment_id if payment_id
       payee_financial_account&.to_xml(xml)
       payment_mandate&.to_xml(xml)
       xml.target!
--- spec/xrechnung/payment_means_spec.rb
+++ spec/xrechnung/payment_means_spec.rb
@@ -5,4 +5,24 @@ RSpec.describe Xrechnung::PaymentMeans do
   it "generates xml" do
     expect_xml_eq_fixture(build_payment_means, "payment_means")
   end
+
+  context "without optional fields" do
+    subject(:doc) { payment_means.to_xml(xml) }
+
+    let(:payment_means) do
+      Xrechnung::PaymentMeans.new(
+        payment_means_code:      30,
+        payee_financial_account: build_payee_financial_account,
+      )
+    end
+    let(:xml) { Builder::XmlMarkup.new(indent: 2) }
+
+    it "generates xml without InstructionNote" do
+      expect(doc).not_to include("InstructionNote")
+    end
+
+    it "generates xml without PaymentID" do
+      expect(doc).not_to include("PaymentID")
+    end
+  end
 end
